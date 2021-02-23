09b5e54c361ef79b4231b479946f3458e9e76977
n-ari <fukunari.r.aa@m.titech.ac.jp>
2021-02-23 22:34:24 +0900

    remove unused dependencies

--- deps.ts
+++ deps.ts
@@ -3,8 +3,4 @@ export type {
   SupportedAlgorithm,
 } from "https://deno.land/std@0.88.0/hash/mod.ts";
 export { createHash } from "https://deno.land/std@0.88.0/hash/mod.ts";
-export {
-  assert,
-  assertEquals,
-} from "https://deno.land/std@0.88.0/testing/asserts.ts";
-export { assertCallbackErrorUncaught } from "https://deno.land/std@0.88.0/node/_utils.ts";
+export { assertEquals } from "https://deno.land/std@0.88.0/testing/asserts.ts";
